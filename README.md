3d7b70c3d87405d37b532f11e2fe03afaf29aee6
silvestrevivo <silvestrevivomillan@gmail.com>
2017-04-25 15:51:37 +0200

    definitive version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-quote-machine
 Random quote machine using Rest API. Exercise for Freecodecamp.
+
+Demo in https://silvestrevivo.github.io/random-quote-machine/
--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-function callQuotes(){
-  $.ajax('http://quotes.stormconsultancy.co.uk/quotes.json', {
-    type: 'GET',
-    data: 'null',
-    dataType: 'json',
-    success: function(data){
-      var randomIndex = Math.floor(Math.random() * data.length);
-      $.each(data, function(i){
-        if(randomIndex === data[i]['id']){
-          $('#quote').text(data[i]['quote']);
-          $('#author').text(data[i]['author']);
-        }
-      });
-    },
-    error: function(request, errorType, errorMessage){
-      alert('Error: ' + errorType + ' with message: ' + errorMessage);
-    }
-  });
-}
-
 $(document).ready(function(){
-  callQuotes();
-  $('.btn').click(callQuotes);
+  //Start point fadeIn of the site
+  $('.quotegroup').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 3000);
+
+  //Ajax Request
+  $('.btn').on('click', function(){
+    $.ajax('http://quotes.stormconsultancy.co.uk/quotes.json', {
+      type: 'GET',
+      data: 'null',
+      dataType: 'json',
+      success: function(data){
+        var randomIndex = Math.floor(Math.random() * data.length);
+        $.each(data, function(i){
+          if(randomIndex === data[i]['id']){
+            $('.quotegroup').fadeOut(1000, function(){
+              $('#quote').text(data[i]['quote']);
+              $('#author').text(data[i]['author']);
+              $(this).fadeIn(1000);
+            });
+          }
+        });
+      },
+      error: function(request, errorType, errorMessage){
+        alert('Error: ' + errorType + ' with message: ' + errorMessage);
+      }
+    });
+  });
+
+  //Change color&background-color
+  $('.btn').on('click', function(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    $('body, .btn').css('background-color', color);
+    $('.quotegroup').find('p').css('color', color);
+    $('.quotegroup').find('footer').css('color', color);
+  });
 });
